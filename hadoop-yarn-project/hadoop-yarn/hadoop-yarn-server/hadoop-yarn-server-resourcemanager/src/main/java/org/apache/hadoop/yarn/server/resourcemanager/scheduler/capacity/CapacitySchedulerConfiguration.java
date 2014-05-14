663b5bbaedd42ef9422242d924e7f83cee54c4d5
Abin Shahab <ashahab@altiscale.com>
2014-05-14 11:43:29 -0700

    DP-709 Revert "Merge pull request #48 from VertiPub/YARN-1512-AsyncScheduling"
    This reverts commit 6eccd940b1df2f0baa1c5f01991675696d5d422f, reversing
    changes made to a566ad531003fada4fe4eb6efbd68c07d71bcda4.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacitySchedulerConfiguration.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacitySchedulerConfiguration.java
@@ -135,17 +135,6 @@ public class CapacitySchedulerConfiguration extends Configuration {
   @Private 
   public static final int DEFAULT_NODE_LOCALITY_DELAY = -1;
 
-  @Private
-  public static final String SCHEDULE_ASYNCHRONOUSLY_PREFIX =
-      PREFIX + "schedule-asynchronously";
-
-  @Private
-  public static final String SCHEDULE_ASYNCHRONOUSLY_ENABLE =
-      SCHEDULE_ASYNCHRONOUSLY_PREFIX + ".enable";
-
-  @Private
-  public static final boolean DEFAULT_SCHEDULE_ASYNCHRONOUSLY_ENABLE = false;
-  
   public CapacitySchedulerConfiguration() {
     this(new Configuration());
   }
@@ -361,14 +350,4 @@ public class CapacitySchedulerConfiguration extends Configuration {
         resourceCalculatorClass, 
         ResourceCalculator.class);
   }
-
-  public boolean getScheduleAynschronously() {
-    return getBoolean(SCHEDULE_ASYNCHRONOUSLY_ENABLE,
-      DEFAULT_SCHEDULE_ASYNCHRONOUSLY_ENABLE);
-  }
-
-  public void setScheduleAynschronously(boolean async) {
-    setBoolean(SCHEDULE_ASYNCHRONOUSLY_ENABLE, async);
-  }
-
 }
